1ec5f5f069a245369ba53334037aaa8edd49a135
Alon <alonmps@icloud.com>
2019-02-20 14:30:41 -0500

    Update main.js
    Added Javascript

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let schedule = [
+  {
+    row1: [
+      {name: "Assignments"}, {name: "Due Date"}, {name: "Status"}],
+    row2: [
+      {name: "C1A2"}, {name: "Week 11"}, {name: "Not started"}],
+    row3: [
+      {name: "C2A3"}, {name: "Week 11"}, {name: "Started"}],
+    row4: [
+      {name: "C3A3"}, {name: "Week 10"}, {name: "Finished"}],
+    row5: [
+      {name: "C4A4"}, {name: "Week 13"}, {name: "Not started"}],
+    row6: [
+      {name: "C5A5"}, {name: "Week 15"}, {name: "Started"}],
+    row7: [
+      {name: "C6A3"}, {name: "Week 10"}, {name: "Finished"}],
+  }
+];
+
+console.log( schedule[1].id );
