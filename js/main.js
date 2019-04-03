5d072266a26ca7dcff38f8d09598262459856119
Stefan Markovic <47187975+stefanmarkovic99@users.noreply.github.com>
2019-04-02 23:08:44 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,13 @@
 let schedule = [
-  {class: "C1", assignment: "A2", due: "Week 11", completion: "Not started"},
-  {class: "C1", assignment: "A2", due: "Week 11", completion: "Not started"},
-
-    {name: "C2A3"}, {name: "Week 11"}, {name: "Started"},
-    row4: [
-      {name: "C3A3"}, {name: "Week 10"}, {name: "Finished"}],
-    row5: [
-      {name: "C4A4"}, {name: "Week 13"}, {name: "Not started"}],
-    row6: [
-      {name: "C5A5"}, {name: "Week 15"}, {name: "Started"}],
-    row7: [
-      {name: "C6A3"}, {name: "Week 10"}, {name: "Finished"}],
-  }
+  {class: "C1", assignment: "A2", due: "Week 11", completion: "Not Started"},
+  {class: "C2", assignment: "A3", due: "Week 11", completion: "Started"},
+  {class: "C3", assignment: "A3", due: "Week 10", completion: "Finished"},
+  {class: "C4", assignment: "A4", due: "Week 13", completion: "Not Started"},
+  {class: "C5", assignment: "A5", due: "Week 15", completion: "Started"},
+  {class: "C6", assignment: "A3", due: "Week 10", completion: "Finished"},
 ];
 
 
-
-
-//
 //document.getElementById('gradebook').innerHTML = "test"
 //console.log( schedule[1].id );
 
