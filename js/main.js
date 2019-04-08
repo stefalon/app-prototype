aa0cb4321333454d69246b0583aba6f4862a521b
Stefan Markovic <47187975+stefanmarkovic99@users.noreply.github.com>
2019-04-08 13:05:26 -0400

    Final JavaScript

--- js/main.js
+++ js/main.js
@@ -1,75 +1,53 @@
 let schedule = [
-  {course: "C1", assignment: "A2", due: "Week 11", completion: "Not Started"},
+  {course: "C1", assignment: "A2", due: "Week 11", completion: "Unbegun"},
   {course: "C2", assignment: "A3", due: "Week 11", completion: "Started"},
   {course: "C3", assignment: "A3", due: "Week 10", completion: "Finished"},
-  {course: "C4", assignment: "A4", due: "Week 13", completion: "Not Started"},
+  {course: "C4", assignment: "A4", due: "Week 13", completion: "Unbegun"},
   {course: "C5", assignment: "A5", due: "Week 15", completion: "Started"},
   {course: "C6", assignment: "A3", due: "Week 10", completion: "Finished"},
 ];
 
 console.log(schedule);
 
-let htmlOutput = schedule.map(sch => `
-  <table>
-  <tr>
-  <th class="marksheader"></th>
-  <th class="marksheader">COURSE</th>
-  <th class="marksheader">ASSIGMENT</th>
-  <th class="marksheader">DUE DATE</th>
-  <th class="marksheader">COMPLETION</th>
-  </tr>
-  <tr>
-    <th></th>
-    <th>${course.0}</th>
-    <th>${assignment.0}</th>
-    <th>${due.0}</th>
-    <th class="work">${completion.0}</th>
-  </tr>
-  <tr>
-    <th></th>
-    <th>${course.1}</th>
-    <th>${assignment.1}</th>
-    <th>${due.1}</th>
-    <th class="study">${completion.1}</th>
-  </tr>
-  <tr>
-    <th></th>
-    <th class="projectdue">${course.2}</th>
-    <th class="projectdue">${assignment.2}</th>
-    <th class="projectdue">${due.2}</th>
-    <th class="projectdue">${completion.2}/th>
-  </tr>
-  <tr>
-    <th></th>
-    <th>${course.3}</th>
-    <th>${assignment.3}</th>
-    <th>${due.3}</th>
-    <th class="relax">${completion.3}</th>
-  </tr>
-  <tr>
-    <th></th>
-    <th>${course.4}</th>
-    <th>${assignment.4}</th>
-    <th>${due.4}</th>
-    <th class="commute">${completion.4}</th>
-  </tr>
-  <tr>
-    <th></th>
-    <th class="projectdue">${course.5}</th>
-    <th class="projectdue">${assignment.5}</th>
-    <th class="projectdue">${due.5}</th>
-    <th class="projectdue">${completion.5}</th>
-  </tr>
-  </table>`).join('');
+let counter = 1;
+let htmlOutput =
+'<table>'+
+'<tr>'+
+'<th class="marksheader"></th>'+
+'<th class="marksheader">COURSE</th>'+
+'<th class="marksheader">ASSIGMENT</th>'+
+'<th class="marksheader">DUE DATE</th>'+
+'<td class="marksheader">COMPLETION</th>'+
+'</tr>'+
+schedule.map(sch =>
+`<tr class="row`+counter++ +`">
+  <td></td>
+  <td>${sch.course}</td>
+  <td>${sch.assignment}</td>
+  <td>${sch.due}</td>
+  <td class="buttonFull">
+<button class="${sch.completion} buttonSize" onclick="myFunction(this)">${sch.completion}</button>
+  </td>
+</tr>`
+); +
+'</table>';
 
 document.getElementById('gradebook').innerHTML = htmlOutput;
 
-//document.getElementById('gradebook').innerHTML = "test"
-//console.log( schedule[1].id );
-
-//schedule.forEach( schdl => {
-//document.getElementById('gradebook').innerHTML += ""
-
-//}
-
-//map the function into empty div
+function  myFunction(button){
+if(button.innerText == "Unbegun"){
+  button.classList.remove("Unbegun")
+  button.classList.add("Started")
+  button.innerText = "Started";
+}
+else if (button.innerText == "Started"){
+  button.classList.remove("Started")
+  button.classList.add("Finished")
+  button.innerText = "Finished";
+}
+else if (button.innerText == "Finished"){
+  button.classList.remove("Finished")
+  button.classList.add("Unbegun")
+  button.innerText = "Unbegun";
+}
+}
